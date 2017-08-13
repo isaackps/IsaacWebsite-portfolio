90f664fb3d4ba689025568748356720eac67c8e3
isaackps <isaac.kps@gmail.com>
2017-08-13 17:27:10 +0800

    done with media query and added profile pic

--- isaacportfoliowebsite/src/Components/About/About.js
+++ isaacportfoliowebsite/src/Components/About/About.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import ProfilePic from '../../isaac.jpg';
 
 import './About.css';
 
@@ -11,7 +12,9 @@ export default class About extends React.Component {
     return (
       <div className="AboutBG" id="About">
         <h1 className="AboutTitle">About Me</h1>
-        <p className="AboutContent">I graduated from The College of Fine Arts (UNSW) with a Bachelor degree in Digital Media. I have a good eye for design and I aspire to be a front end web developer. For the past 2 years, I have been working alongside my wife and building our business: My Sister Bakes. My work has given me experience and great exposure to lead a team of staff,  to communicate and work in teams, as well as being able to make independent business decisions as a boss. Now that the business is stable, I have decided to pursue my interest and venture into another industry (web development) that also requires the use of digital media.</p>
+        <img src={ProfilePic} className="profilePic" />
+        <p className="AboutContent">I graduated from The College of Fine Arts (UNSW) with a Bachelor degree in Digital Media. I have a good eye for design and I aspire to be a front end web developer. For the past 2 years, I have been working alongside my wife and building our business: My Sister Bakes. </p>
+        <p className="AboutContent">My work has given me experience and great exposure to lead a team of staff,  to communicate and work in teams, as well as being able to make independent business decisions as a boss. Now that the business is stable, I have decided to pursue my interest and venture into another industry (web development) that also requires the use of digital media.</p>
       </div>
     );
   }
